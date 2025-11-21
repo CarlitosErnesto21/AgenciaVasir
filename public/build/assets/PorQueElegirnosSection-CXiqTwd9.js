de1c1f38144c6c827f2d83bb98dfb5717137b16c
CarlitosErnesto21 <ernesto.rosales354@gmail.com>
2025-11-20 21:39:56 -0600

    Modificaciones de interfaces

---
+++ public/build/assets/PorQueElegirnosSection-CXiqTwd9.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./PorQueElegirnosSection.vue_vue_type_script_setup_true_lang-DyePtC-m.js";import"./index-KhYYztup.js";import"./ui-CVJOYm3g.js";import"./vendor-CqKcGHPa.js";export{o as default};
